ce582c8a9f3d9370180f3434376aa59cba378c75
matthewjgarcia <matthewjggarcia@gmail.com>
2023-12-12 21:32:46 -0800

    Finished app implementation

--- hawaii_trip/app.py
+++ hawaii_trip/app.py
@@ -47,7 +47,8 @@ def precipitation():
     session = Session(engine)
     one_year = dt.date(2017, 8, 23)- dt.timedelta(days= 365)
     prev_year = dt.date(one_year.year, one_year.month, one_year.day)
-    query = session.query(measurement.date, measurement.prcp).filter(measurement.date >= prev_year).order_by(measurement.date).all()
+    query = session.query(measurement.date, measurement.prcp).filter(measurement.date >= prev_year)\
+        .order_by(measurement.date).all()
     precip_dict = {date: prcp for date, prcp in query}
     return jsonify(precip_dict)
 
@@ -72,6 +73,39 @@ def tobs():
 
     return jsonify(tobs)
 
+@app.route("/api/v1.0/<start>")
+def temps_start(start):
+    session = Session(engine)
+    query = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+            filter(measurement.date >= start).all()
+    session.close()
+
+    temps = []
+    for min_temp, avg_temp, max_temp in query:
+        temps_dict = {}
+        temps_dict['Minimum Temperature'] = min_temp
+        temps_dict['Average Temperature'] = avg_temp
+        temps_dict['Maximum Temperature'] = max_temp
+        temps.append(temps_dict)
+
+    return jsonify(temps)
+
+@app.route("/api/v1.0/<start>/<end>")
+def temps_start_end(start, end):
+    session = Session(engine)
+    query = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs)).\
+              filter(measurement.date >= start).filter(measurement.date <= end).all()
+    session.close()
+
+    temps = []
+    for min_temp, avg_temp, max_temp in query:
+        temps_dict = {}
+        temps_dict['Minimum Temperature'] = min_temp
+        temps_dict['Average Temperature'] = avg_temp
+        temps_dict['Maximum Temperature'] = max_temp
+        temps.append(temps_dict)
+
+    return jsonify(temps)
 
 if __name__ == '__main__':
     app.run(debug=True)
